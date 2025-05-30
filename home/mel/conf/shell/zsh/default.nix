dfabbe0d851cfc67dad71e7f9eef71209d22ddb9
melano qtx <vaibhavmalik1127@gmail.com>
2025-05-30 01:56:39 -0400

    vedika

--- home/mel/conf/shell/zsh/default.nix
+++ home/mel/conf/shell/zsh/default.nix
@@ -3,7 +3,7 @@
 {
   programs.zsh = {
     enable = true;
-    Autosuggestions = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     shellAliases = {
