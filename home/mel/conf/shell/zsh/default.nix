2594e9a8b67e0a53823de16bba8dae1e6abeecbb
melano qtx <vaibhavmalik1127@gmail.com>
2025-05-30 03:41:56 -0400

    rofi next

--- home/mel/conf/shell/zsh/default.nix
+++ home/mel/conf/shell/zsh/default.nix
@@ -1,5 +1,4 @@
 { config, colors, pkgs, lib, ... }:
-
 {
   programs.zsh = {
     enable = true;
@@ -27,10 +26,13 @@
       export PATH=${config.home.homeDirectory}/.local/bin:${config.home.homeDirectory}/.local/share/nvim/mason/bin:$PATH
       export COLS=${colors.background}\ ${colors.foreground}\ ${colors.color1}\ ${colors.color2}\ ${colors.color3}\ ${colors.color4}\ ${colors.color5}\ ${colors.color6}\ ${colors.color7}\ ${colors.color8}\ ${colors.color9}\ ${colors.color10}\ ${colors.color11}\ ${colors.color12}\ ${colors.color13}\ ${colors.color14}\ ${colors.color15}\ ${colors.mbg}\ ${colors.color0}\ ${colors.comment}
       export LD_LIBRARY_PATH=${config.home.homeDirectory}/.config/awesome:${pkgs.lua54Packages.lua}/lib/:${pkgs.pam}/lib
-      . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      
+      # Only source if the file exists
+      if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      fi
     '';
   };
-
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
@@ -42,3 +44,5 @@
     };
   };
 }
+
+
